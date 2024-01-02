87b9a35deb42f99f12a1f9ee633670bd9fa34532
Ansar Saeed <89566908+Ansar695@users.noreply.github.com>
2024-01-02 11:39:28 +0500

    shadcn integrated with vite reactjs

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { Button } from "./components/ui/button"
 function App() {
 
   return (
-    <div>
+    <div className="p-5">
+      <h1 className="text-5xl">Shadcn Integrated with React JS Vite.</h1>
       <Button variant="secondary" className="bg-dark">Click Me</Button>
       <Badge variant="outline">Badge</Badge>
     </div>
