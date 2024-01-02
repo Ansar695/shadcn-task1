97ba7446c341b582251d21d717e4f07c58cb9bb0
Ansar Saeed <89566908+Ansar695@users.noreply.github.com>
2024-01-02 11:31:22 +0500

    shadcn integrated

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Badge } from "./components/ui/badge"
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div>
+      <Button variant="secondary" className="bg-dark">Click Me</Button>
+      <Badge variant="outline">Badge</Badge>
+    </div>
+  )
+}
+
+export default App
